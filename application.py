e82e0477e13e3173b26bed75c51cb06c00a58301
saliou seck <saliouseck2009@gmail.com>
2020-07-12 08:27:02 +0000

    Configuring the login page

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import os
 
-from flask import Flask
+from flask import Flask,render_template,request,flash
 from flask_socketio import SocketIO, emit
+from env import Config
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+#app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+app.config.from_object(Config)
 socketio = SocketIO(app)
 
 
-@app.route("/")
-def index():
-    return "Project 2: TODO"
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        pseudo = request.form['pseudo']
+        return pseudo
+
+
+@app.route('/login')
+def login():
+
+    return render_template('login.html')
